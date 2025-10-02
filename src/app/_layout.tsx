57870974dd1ec3b5bfb7d5932c6628a483a77b57
matheusclima <matheuslima@pwrgestao.com>
2025-10-02 14:11:53 -0300

    feat: integrate react-query for contract management and enhance API service
    - Added QueryClientProvider to MainLayout for react-query integration.
    - Created api service using axios with dynamic base URL from environment variables.
    - Developed AdminContractScreen to manage contract states with tabs for "Pendentes", "Aceitos", and "Recusados".
    - Implemented ContractCard component to display contract details and actions.
    - Defined Contract type with additional fields for dateTime, location, and price.
    - Updated package.json and package-lock.json to include new dependencies: @tanstack/react-query and axios.
    - Added .env.example for environment variable configuration.
    - Updated .gitignore to include local environment files.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/global.css";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient();
 export default function MainLayout() {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }}></Stack>
+    </QueryClientProvider>
+  );
 }
