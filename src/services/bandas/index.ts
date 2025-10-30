89b5ee705a1fec3e7df89ebd842f3cf06df5e663
Paulo Luan <pauloluan1409@gmail.com>
2025-10-30 17:12:16 -0300

    feat: ajustando navegação

--- src/services/bandas/index.ts
+++ src/services/bandas/index.ts
@@ -8,3 +8,12 @@ export async function getBandById(id: string) {
     throw error;
   }
 }
+
+export async function getBandByUserId(userId: string) {
+  try {
+    const response = await api.get(`/bands/user/${userId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
